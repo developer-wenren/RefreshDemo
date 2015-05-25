0f2a386c682fe72f9fc8fb9ecaf52e0ff78eae20
wrcj <wrcj12138@163.com>
2015-05-25 16:50:07 +0800

    Initial Commit

---
+++ RefreshDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshDemo
+//
+//  Created by zjsruxxxy3 on 15/5/25.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
